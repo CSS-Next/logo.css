4c744e4677ada8ce007c920114aa92a0052c0fb0
Quentin Albert <quentin.albert@web.de>
2024-11-13 19:52:18 +0100

    Add asset build process with sharp Add 2x versions of all formats and files Add .ico of all files Add .svg for square

---
+++ assets.mjs
@@ -0,0 +1,46 @@
+import sharp from "npm:sharp@0.33.5";
+
+// Names of the SVGs to use for generation
+const logoFileNames = ["css", "css.square", "css.dark", "css.light"];
+
+// Start the generation process for each logo
+for (const logoFileName of logoFileNames) {
+  // Extract the type of the logo, when the type is not provided it defaults to "primary"
+  const [_, logoType = "primary"] = logoFileName.split(".");
+
+  // Load the SVG file into sharp
+  const image = await sharp(`${logoFileName}.svg`);
+
+  // Specify and prepare the output folder
+  const outputFolder = `./${logoType}`;
+
+  // Remove the output folder if it exists
+  try {
+    await Deno.remove(outputFolder, { recursive: true });
+  } catch (error) {
+    if (!(error instanceof Deno.errors.NotFound)) {
+      throw error;
+    }
+  }
+
+  await Deno.mkdir(outputFolder);
+
+  const formats = ["png", "webp", "avif", "jpeg"];
+
+  // Generate the different formats for the logo
+  for (const format of formats) {
+    const result = image.toFormat(format, {
+      lossless: true,
+    });
+
+    result.toFile(`${outputFolder}/${logoFileName}.${format}`);
+
+    result
+      .resize(2000, 2000)
+      .toFile(`${outputFolder}/${logoFileName}.2x.${format}`);
+
+    if (format === "png") {
+      result.resize(32, 32).toFile(`${outputFolder}/${logoFileName}.ico`);
+    }
+  }
+}
