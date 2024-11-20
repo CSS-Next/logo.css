1d1758a6815e441928383e86283c317556f9bf76
Max Duval <max.duval@hometrack.com>
2024-11-20 09:07:51 +0000

    feat: programmatically generate variants
    - light
    - dark
    - square
    
    parsing the source SVG as a document and updating
    the relevant elements and their content.

--- assets.mjs
+++ assets.mjs
@@ -2,14 +2,61 @@
 // @ts-check
 
 import sharp from "npm:sharp@0.33.5";
+import { DOMParser } from "npm:linkedom@0.18.5";
+
+/** the text contained in the source SVG file */
+const text = await Deno.readTextFile("css.svg");
 
 // Names of the SVGs to use for generation
 const logoFileNames = ["css", "css.square", "css.dark", "css.light"];
 
 // Start the generation process for each logo
 for (const logoFileName of logoFileNames) {
+  const svg = new DOMParser().parseFromString(text, "image/svg+xml");
+
+  const fg = svg.getElementById("fg");
+  const bg = svg.getElementById("bg");
+  const title = svg.getElementById("css-logo-title");
+  const description = svg.getElementById("css-logo-description");
+
+  switch (logoFileName) {
+    case "css": {
+      // this is the source logo, do nothing!
+      break;
+    }
+    case "css.light": {
+      title.innerHTML = "CSS Logo Light";
+      description.innerHTML =
+        "A white square with rounded corners and the letters CSS inside in black";
+      fg.setAttribute("fill", "black");
+      bg.setAttribute("fill", "white");
+      break;
+    }
+    case "css.dark": {
+      title.innerHTML = "CSS Logo Dark";
+      description.innerHTML =
+        "A black square with rounded corners and the letters CSS inside in white";
+      fg.setAttribute("fill", "white");
+      bg.setAttribute("fill", "black");
+      break;
+    }
+    case "css.square": {
+      title.innerHTML = "CSS Logo Square";
+      description.innerHTML =
+        "A purple square with the letters CSS inside in white";
+      bg.setAttribute("d", "M0,0H1000V1000H-1000Z");
+      break;
+    }
+  }
+
   // Extract the type of the logo, when the type is not provided it defaults to "primary"
   const [logoName, logoType = "primary"] = logoFileName.split(".");
+
+  // overwrite the SVG files with the updated content
+  await Deno.writeTextFile(
+    `${logoFileName}.svg`,
+    svg.toString().replace('<?xml version="1.0" encoding="utf-8"?>', ""),
+  );
 
   // Load the SVG file into sharp
   const image = await sharp(`${logoFileName}.svg`);
